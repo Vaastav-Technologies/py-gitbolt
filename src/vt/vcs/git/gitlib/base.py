26ffb4abf4cf62f1f7967e8e73e538703a0c2eb9
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-05-27 22:36:05 +0530

    corrected datatype.

--- src/vt/vcs/git/gitlib/base.py
+++ src/vt/vcs/git/gitlib/base.py
@@ -42,7 +42,7 @@ class GitOpts(TypedDict, total=False):
     `Documented <https://git-scm.com/docs/git#Documentation/git.txt--Cltpathgt>`_.
     """
 
-    c: dict[str, str | None | Unset] | None | Unset
+    c: dict[str, str | bool | None | Unset] | None | Unset
     """
     Mirror of ``-c <name>=<value>``.
 
