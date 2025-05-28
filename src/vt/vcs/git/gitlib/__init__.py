36dd0e5ee54db2679f0be0a261596917e6f22912
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-05-28 13:51:03 +0530

    Project's own `errmsg_creator` introduced.

--- src/vt/vcs/git/gitlib/__init__.py
+++ src/vt/vcs/git/gitlib/__init__.py
@@ -6,3 +6,12 @@ Git command interfaces with default implementation using subprocess calls.
 """
 
 from vt.vcs.git.gitlib.base import *
+
+from vt.utils.errors.error_specs import ErrorMsgFormer
+
+errmsg_creator = ErrorMsgFormer
+"""
+Create formatted error messages using this global instance.
+
+To get a local instance use ``errmsg_creator.clone_with(...)``.
+"""
