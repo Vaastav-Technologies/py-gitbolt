72fddcd0acabcf4338f65091310445308a44402c
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-11-06 20:25:17 +0530

    cli: grouped only ctor tests.

--- test/test_gitbolt/test_git_subprocess/test_impl/test_simple.py
+++ test/test_gitbolt/test_git_subprocess/test_impl/test_simple.py
@@ -640,15 +640,20 @@ class TestMainCLIGit:
     """
     Test for ``CLISimpleGitCommand``.
     """
-    @pytest.mark.parametrize("inouts", [
-        [],
-        ["--no-replace-objects"],
-        ["--paginate", "--git-dir", ".", "--no-replace-objects"],
-        ["--"]
-    ])
-    def test_ctor_call(self, inouts):
-        git = CLISimpleGitCommand(opts=inouts)
-        assert inouts == git.build_main_cmd_args()
+    class TestOnlyCtorCall:
+        """
+        Tests where the values are directly and exclusively set on ctor and no other setting operation is performed
+        after that.
+        """
+        @pytest.mark.parametrize("opts", [
+            [],
+            ["--no-replace-objects"],
+            ["--paginate", "--git-dir", ".", "--no-replace-objects"],
+            ["--"]
+        ])
+        def test_cmd_call(self, opts):
+            git = CLISimpleGitCommand(opts=opts)
+            assert opts == git.build_main_cmd_args()
 
     class TestNoArgCtorCall:
         def test_main_cmd_args(self):
