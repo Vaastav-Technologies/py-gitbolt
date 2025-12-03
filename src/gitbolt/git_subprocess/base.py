bfc5e7a99376cce11efedbd6d75126b9b8b3c91d
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-12-04 01:21:45 +0530

    Respect user supplied `check` kwarg
    when running `UncheckedSubcmd`, respect user's `check=False` to avoid raising `CalledProcessError` when underlying process fails.

--- src/gitbolt/git_subprocess/base.py
+++ src/gitbolt/git_subprocess/base.py
@@ -581,7 +581,7 @@ class UncheckedSubcmd(GitSubcmdCommand, RootDirOp, Protocol):
             envs_vars.update(another_supplied_env)
         cwd = subprocess_run_kwargs.pop("cwd", None) or self.root_dir
         capture_output = subprocess_run_kwargs.pop("capture_output", None) or True
-        check = subprocess_run_kwargs.pop("check", None) or True
+        check = subprocess_run_kwargs.pop("check", True)
         # Run the git command
         result = self.underlying_git.runner.run_git_command(
             main_cmd_args,
