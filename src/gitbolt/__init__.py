8f2144f109a669153f12f9684104e723c5d841dd
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-12-05 13:20:00 +0530

    checking version.version from unchecked

--- src/gitbolt/__init__.py
+++ src/gitbolt/__init__.py
@@ -34,7 +34,7 @@ def get_git(git_root_dir: Path = Path.cwd()) -> Git:
 
     >>> import subprocess
     >>> from gitbolt.git_subprocess.impl.simple import SimpleGitCommand
-    >>> SimpleGitCommand().version().version()
+    >>> SimpleGitCommand().subcmd_unchecked.run(["version"])
 
     :param git_root_dir: Path to the git repo root directory. Defaults to current working directory.
     """
