fab621a8f1bb79b137bcd3506511d8c5f3330e25
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-12-05 14:07:22 +0530

    Checking if env-vars change things

--- src/gitbolt/__init__.py
+++ src/gitbolt/__init__.py
@@ -36,7 +36,7 @@ def get_git(git_root_dir: Path = Path.cwd()) -> Git:
     >>> import subprocess
     >>> import pathlib
     >>> from gitbolt.git_subprocess.impl.simple import SimpleGitCommand
-    >>> subprocess.run(["git", "version"], capture_output=True, env=os.getenv())
+    >>> subprocess.run(["git", "version"], capture_output=True, env=os.environ)
 
     :param git_root_dir: Path to the git repo root directory. Defaults to current working directory.
     """
