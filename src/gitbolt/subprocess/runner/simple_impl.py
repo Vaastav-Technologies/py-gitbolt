5ec54d9ff627da079523de3774c615f90b82761f
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2025-12-29 13:17:38 +0530

    `git_prog()` property required.
    This is done so that users can set their own git command runners and hence, set th git command's location/ separate git command version/ a git command emulator.

--- src/gitbolt/subprocess/runner/simple_impl.py
+++ src/gitbolt/subprocess/runner/simple_impl.py
@@ -7,6 +7,7 @@ A simple and straight-forward git command subprocess runner implementation.
 
 from __future__ import annotations
 
+import pathlib
 import subprocess
 from subprocess import CompletedProcess
 from typing import overload, override, Any, Literal
@@ -20,6 +21,8 @@ class SimpleGitCR(GitCommandRunner):
     """
     Simple git command runner that simply runs everything `as-is` in a subprocess.
     """
+    def __init__(self, git_prog: str | pathlib.Path = GIT_CMD):
+        self._git_prog = git_prog
 
     @overload
     @override
@@ -79,7 +82,7 @@ class SimpleGitCR(GitCommandRunner):
     ) -> CompletedProcess[str] | CompletedProcess[bytes]:
         try:
             return subprocess.run(
-                [GIT_CMD, *main_cmd_args, *subcommand_args],
+                [str(self.git_prog), *main_cmd_args, *subcommand_args],
                 *subprocess_run_args,
                 input=_input,
                 text=text,
@@ -89,3 +92,8 @@ class SimpleGitCR(GitCommandRunner):
             raise GitCmdException(
                 e.stderr, called_process_error=e, exit_code=e.returncode
             ) from e
+
+    @override
+    @property
+    def git_prog(self) -> str | pathlib.Path:
+        return self._git_prog
