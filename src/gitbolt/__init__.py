5e91f76cbce923bec0a7730f8d61bb4391ec604a
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2026-05-27 15:28:01 +0530

    session: re-export `GitSession`

--- src/gitbolt/__init__.py
+++ src/gitbolt/__init__.py
@@ -16,7 +16,8 @@ from gitbolt.base import GitSubCommand as GitSubCommand
 from gitbolt.base import LsTree as LsTree
 from gitbolt.base import Version as Version
 from gitbolt.base import Add as Add
-from gitbolt.subprocess.base import GitCommand, GitSession
+from gitbolt.subprocess.base import GitCommand
+from gitbolt.subprocess.base import GitSession as GitSession
 # endregion
 
 
