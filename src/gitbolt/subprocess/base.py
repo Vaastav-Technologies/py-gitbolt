fae4a12b595833e8864f7785960cc2bbcf8ae65f
Suhas Srivastava <suhas.srivastava@vaastav.tech>
2026-08-05 20:36:34 +0530

    git: faulty doctest removed

--- src/gitbolt/subprocess/base.py
+++ src/gitbolt/subprocess/base.py
@@ -1165,40 +1165,6 @@ class WorktreeCommand(Worktree, GitSubcmdCommand, abc.ABC):
 
         @override
         def __str__(self) -> str:
-            """
-            >>> import gitbolt
-
-            No options and envs:
-
-            >>> _a_git = gitbolt.get_git_command()
-            >>> assert str(_a_git.worktree_subcmd().add_subcmd()) == f"{GIT_CMD} {WORKTREE_CMD} {WORKTREE_ADD_CMD}"
-
-            Added main command options:
-
-            >>> _b_git = _a_git.git_opts_override(C=[Path("a"), Path("b")], no_advice=True, no_replace_objects=True)
-            >>> assert str(_a_git.worktree_subcmd().add_subcmd()) == f"{GIT_CMD} {WORKTREE_CMD} {WORKTREE_ADD_CMD}"    # _a_git never changed
-            >>> str(_b_git.worktree_subcmd().add_subcmd())
-            >>> assert str(_b_git.worktree_subcmd().add_subcmd()) == f"{GIT_CMD} -C a -C b --no-replace-objects --no-advice {WORKTREE_CMD} {WORKTREE_ADD_CMD}"
-
-            Adding git envs:
-
-            >>> _c_git = _a_git.git_envs_override(GIT_ADVICE=False, GIT_AUTHOR_NAME="Suhas", GIT_PAGER="vi")
-            >>> assert str(_a_git.worktree_subcmd().add_subcmd()) == f"{GIT_CMD} {WORKTREE_CMD} {WORKTREE_ADD_CMD}"    # _a_git never changed
-            >>> str(_c_git.worktree_subcmd().add_subcmd())
-            >>> assert str(_c_git.worktree_subcmd().add_subcmd()) == f"GIT_ADVICE=False GIT_AUTHOR_NAME=Suhas GIT_PAGER=vi {GIT_CMD} {WORKTREE_CMD} {WORKTREE_ADD_CMD}"
-
-            >>> _d_git = _c_git.git_opts_override(C=[Path("a"), Path("b")], no_advice=True, no_replace_objects=True, config_env=dict(conf1="val1", glob1="val2"))
-            >>> str(_d_git.worktree_subcmd().add_subcmd())
-            >>> assert str(_d_git.worktree_subcmd().add_subcmd()) == f"GIT_ADVICE=False GIT_AUTHOR_NAME=Suhas GIT_PAGER=vi {GIT_CMD} -C a -C b --config-env conf1=val1 --config-env glob1=val2 --no-replace-objects --no-advice {WORKTREE_CMD} {WORKTREE_ADD_CMD}"
-
-            >>> _e_git = _d_git.git_opts_override(C=[Path("c"), Path("d")], no_advice=True, no_replace_objects=False, config_env=dict(conf1="val2", glob1="val1"))
-            >>> str(_e_git.worktree_subcmd().add_subcmd())
-            >>> assert str(_d_git.worktree_subcmd().add_subcmd()) == f"GIT_ADVICE=False GIT_AUTHOR_NAME=Suhas GIT_PAGER=vi {GIT_CMD} -C c -C d --config-env conf1=val2 --config-env glob1=val1 {WORKTREE_CMD} {WORKTREE_ADD_CMD}"
-
-            Does not affect repr:
-
-            >>> assert repr(_d_git) != str(_d_git)
-            """
             return " ".join([str(self.underlying_worktree), WORKTREE_ADD_CMD])
 
         @override
